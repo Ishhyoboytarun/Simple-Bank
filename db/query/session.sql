a642a8de3fdb65408d71bc949a6b704951c38da8
TarunnGusain <prudent.tarun0808@gmail.com>
2023-06-08 08:16:55 +0530

    Adding relevant changes

--- db/query/session.sql
+++ db/query/session.sql
@@ -15,7 +15,19 @@ INSERT INTO sessions (
 create table sessions
 (
     id int not null,
-    $1 int not null
+    $1 int not null,
+    username varchar,
+    $2 int not null,
+    refresh_token varchar,
+    $3 int not null,
+    user_agent varchar,
+    $4 int not null,
+    client_ip varchar,
+    $5 int not null,
+    is_blocked bool,
+    $6 int not null,
+    expires_at timestamp,
+    $7 int not null
 );
 
 SELECT * FROM sessions
